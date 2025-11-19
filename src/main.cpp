f42d6421f18d9e94a335b39f2401339b6004beb1
DeviceBlack <233244853+odeviceblack@users.noreply.github.com>
2025-11-18 21:23:37 -0300

    Try forcing fullscreen mode.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <inttypes.h>
 #include <logger.h>
 
-MYMOD(SCAndSkip, ExitSocialClub, 1.0, DeviceBlack)
+MYMOD(SCAndSkip, SCAndSkip, 1.0, DeviceBlack)
 
 //------------------------//
 // Global variables
@@ -15,6 +15,7 @@ void* h_SCAnd = nullptr;
 void ExitSocialClub();
 void SkipEULA();
 void HookLoadScreen();
+void EnableFullScreen();
 
 //------------------------//
 // Hook da função SocialClub::LoadScreen
@@ -23,6 +24,8 @@ DECL_HOOKv(SocialClub_LoadScreen, void* self)
 {
 	// Chama a função original
 	SocialClub_LoadScreen(self);
+	EnableFullScreen();
+
 	logger->Info("SocialClub_LoadScreen => Jumping to main menu!");
 
 	JNIEnv* env = aml->GetJNIEnvironment();
@@ -95,6 +98,69 @@ void ExitSocialClub()
 	HookLoadScreen();
 }
 
+void EnableFullScreen()
+{
+	JNIEnv* env = aml->GetJNIEnvironment();
+
+	// --- Obter classe GTASA ---
+	jclass gtasaClass = env->FindClass("com/rockstargames/gtasa/GTASA");
+	jfieldID selfField = env->GetStaticFieldID(gtasaClass, "gtasaSelf", "Lcom/rockstargames/gtasa/GTASA;");
+	jobject activityObj = env->GetStaticObjectField(gtasaClass, selfField);
+	jclass activityClass = env->GetObjectClass(activityObj);
+
+	// getWindow()
+	jmethodID getWindow = env->GetMethodID(activityClass, "getWindow", "()Landroid/view/Window;");
+	jobject windowObj = env->CallObjectMethod(activityObj, getWindow);
+	jclass windowClass = env->GetObjectClass(windowObj);
+
+	if(android_get_device_api_level() < 12)
+	{
+		// getDecorView()
+		jmethodID getDecorView = env->GetMethodID(windowClass, "getDecorView", "()Landroid/view/View;");
+		jobject decorViewObj = env->CallObjectMethod(windowObj, getDecorView);
+	
+		// LEGACY FLAGS (ainda funcionam no Android 15)
+		const int uiFlags =
+			0x00000004 |  // FULLSCREEN
+			0x00000400 |  // HIDE_NAVIGATION
+			0x00001000 |  // IMMERSIVE_STICKY
+			0x00000200;   // LAYOUT_HIDE_NAVIGATION
+	
+		jclass decorClass = env->GetObjectClass(decorViewObj);
+		jmethodID setSystemUiVisibility = env->GetMethodID(decorClass, "setSystemUiVisibility", "(I)V");
+		env->CallVoidMethod(decorViewObj, setSystemUiVisibility, uiFlags);
+	}
+	else
+	{
+		// --- ANDROID 12–15 MODE (WindowInsetsController) ---
+		jmethodID getInsetsController = env->GetMethodID(windowClass, "getInsetsController", "()Landroid/view/WindowInsetsController;");
+		jobject insetsController = env->CallObjectMethod(windowObj, getInsetsController);
+	
+		if (insetsController)
+		{
+			jclass insetsClass = env->GetObjectClass(insetsController);
+	
+			// hide(WindowInsets.Type.statusBars() | WindowInsets.Type.navigationBars())
+			jclass windowInsetsType = env->FindClass("android/view/WindowInsets$Type");
+			jmethodID statusBars = env->GetStaticMethodID(windowInsetsType, "statusBars", "()I");
+			jmethodID navigationBars = env->GetStaticMethodID(windowInsetsType, "navigationBars", "()I");
+	
+			int types = env->CallStaticIntMethod(windowInsetsType, statusBars) | env->CallStaticIntMethod(windowInsetsType, navigationBars);
+	
+			jmethodID hide = env->GetMethodID(insetsClass, "hide", "(I)V");
+			env->CallVoidMethod(insetsController, hide, types);
+	
+			// setSystemBarsBehavior(BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE)
+			jmethodID setBehavior = env->GetMethodID(insetsClass, "setSystemBarsBehavior", "(I)V");
+
+			int behavior = 2; // BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+			env->CallVoidMethod(insetsController, setBehavior, behavior);
+		}
+	}
+	
+	logger->Info("The statusBars and navigationBars have been hidden!");
+}
+
 //------------------------//
 // Pré-carregamento do mod
 //------------------------//
@@ -102,12 +168,12 @@ ON_MOD_PRELOAD()
 {
 	// Configurações do logger
 	logger->SetTag(PROJECT_NAME_STR);
-
+	
 	/*
-	 * logger->SetFile(aml->GetConfigPath(), "log_SCAndSkip.txt");
-	 * logger->EnableFileLogging(true);
-	 * logger->Info("File logging started!");
-	 */
+	logger->SetFile(aml->GetConfigPath(), "log_SCAndSkip.txt");
+	logger->EnableFileLogging(true);
+	logger->Info("File logging started!");
+	*/
 
 	// Inicializa o mod
 	ExitSocialClub();
